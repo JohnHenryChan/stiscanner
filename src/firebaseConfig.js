64607e61d0051ef06bb786ac5566ec53c002b80c
JohnHenryChan <chan.316817@vigan.sti.edu.ph>
2025-05-22 19:24:09 +0800

    Update firebaseConfig.js
    redacting firebase API Key as a fi

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -10,13 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAiz6AHSScMvXEsxuzTyzoLLni0TInwoQA",
-  authDomain: "stiscanner.firebaseapp.com",
-  projectId: "stiscanner",
-  storageBucket: "stiscanner.firebasestorage.app",
-  messagingSenderId: "506209465614",
-  appId: "1:506209465614:web:f3e6dd95a00eb3b8c8d35f",
-  measurementId: "G-NGR239EKHE"
+  //firebaseConfigsetupreplace here;
 };
 
 // Initialize Firebase
